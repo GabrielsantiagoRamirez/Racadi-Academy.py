8e1c5ce31d4105c6de43185cf0c20a4392d9450e
GabrielsantiagoRamirez <gabrielsabtiago716@gmail.com>
2024-09-03 18:58:24 -0500

    Se modifico el app

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,15 @@ import Login from './components/Login.vue';
 import Pagos from './components/Pagos.vue';
 import Footer from './components/footer.vue';
 import HeaderRacadi from './components/header-Racadi.vue';
+import informacion_pago from './components/informacion_pago.vue';
 </script>
 
 
 
-
-</script>
 <template>
     <!--<Pagos/> -->
     <!--<Login/>-->
     <!--<HeaderRacadi/>-->
-    <pagina_pago_banco/>
 </template>
 
  
