fd2ca4536eb5444cded8bff4b9aa4f1f5533e866
Oscar Diaz <oscaritho2005@gmail.com>
2024-09-12 19:34:00 -0500

    fix damages

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,12 @@ import Login from './components/Login.vue';
 import Pagos from './components/Pagos.vue';
 import Main from './components/Main.vue';
 import pagina_pago_banco from './components/pagina_pago_banco.vue';
-import Pagina_pago_banco from './components/pagina_pago_banco.vue';
 import informacion_pago from './components/informacion_pago.vue';
-import Informacion_pago from './components/informacion_pago.vue';
 </script>
 
 <template>
-
 </template>
-
- 
 
 
 <style scoped>
-
 </style>
