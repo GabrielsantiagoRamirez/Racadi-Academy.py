85c91097d30efe20488458757552ef5e5a885cfb
sebas1531 <sebas15311@gmail.com>
2024-09-24 17:26:33 -0500

    Descripción de los cambios

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@ import Pagos from './components/Pagos.vue';
 import Main from './components/Main.vue';
 import pagina_pago_banco from './components/pagina_pago_banco.vue';
 import informacion_pago from './components/informacion_pago.vue';
+import perfil from './components/perfil.vue';
 </script>
 
 <template>
       <!--<pagina_pago_banco/>-->
-      <Main/>
+    <!--<Main/>-->  
+    <perfil/>
 </template>
 
 
