a27610c940688546a639f8c5746395b10e9e579b
Oscar Diaz <oscaritho2005@gmail.com>
2024-09-24 23:05:51 -0500

    merge with master

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
 <script setup>
-import Login from './components/Login.vue';
-import Pagos from './components/Pagos.vue';
-import Main from './components/Main.vue';
-import pagina_pago_banco from './components/pagina_pago_banco.vue';
-import informacion_pago from './components/informacion_pago.vue';
-import perfil from './components/perfil.vue';
-
 </script>
 
 
